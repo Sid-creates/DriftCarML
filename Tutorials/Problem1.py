0eb8233434ad939062a00e51dd463754bdbb13a9
Sid-creates <siddhardach@outlook.com>
2024-02-23 04:57:44 -0500

    problem1 added test cases

--- Tutorials/Problem1.py
+++ Tutorials/Problem1.py
@@ -15,3 +15,13 @@ my_list = [10, 5, 8, 3, 12, 15]
 result = find_range(my_list)
 print(f"The range of the list is: {result}")
 # Expected output: "The range of the list is: 12" (since max - min = 15 - 3 = 12)
+
+# test cases
+print(find_range([1, 2, 3, 4, 5])) # 4
+print(find_range([5, 4, 3, 2, 1])) # 4
+print(find_range([1, 2, 3, 4, 5, 6])) # 5
+print(find_range([6, 5, 4, 3, 2, 1])) # 5
+print(find_range([1, 1, 1, 1, 1])) # 0
+print(find_range([1, 1, 1, 1, 1, 1])) # 0
+print(find_range([1])) # 0
+print(find_range([])) # "List is empty. Cannot calculate range."
